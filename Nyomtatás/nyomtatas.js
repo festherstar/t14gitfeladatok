13e203330f65247f994b3d4d9ee2968261cb31a1
festherstar <f.esther94@gmail.com>
2024-03-09 10:21:23 +0100

    Nyomtatás feladat

---
+++ Nyomtatás/nyomtatas.js
@@ -0,0 +1,17 @@
+function kalkulal(){
+    
+    //Űrlapadatok
+    const szelesseg=document.querySelector("#szelesseg").value;
+    const magassag=document.querySelector("#magassag").value;
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet=Math.round((szelesseg*magassag)/10000);    
+    let koltseg=terulet*papir;
+    //Megjelenítés
+    document.getElementById('terulet').innerHTML=terulet;
+    document.getElementById('papir').innerHTML=papir;
+    document.getElementById('koltseg').innerHTML=koltseg;
+    document.getElementById('valasz').style.visibility = "visible";
+}
+
+
